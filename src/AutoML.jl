ef743ccc25820b33087b64a665451fb24e000eee
guolanting <guolanting@haplox.com>
2016-01-04 12:41:52 +0000

    basic architecture

--- src/AutoML.jl
+++ src/AutoML.jl
@@ -1,9 +1,13 @@
 module AutoML
 
-# package code goes here
+using XGBoost
+using MLBase
 
 
 
-
+include("preprocess/preprocess.jl")
+include("feature_engineer/feature_engineer.jl")
+include("model/model.jl")
+include("evaluation/evalation.jl")
 
 end # module
