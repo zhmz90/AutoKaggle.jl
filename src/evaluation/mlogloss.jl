f2493fe5c8375b243aaabb39b55901e5e1d6086c
guolanting <guolanting@haplox.com>
2016-01-05 04:54:17 +0000

    make every element of array to be float

--- src/evaluation/mlogloss.jl
+++ src/evaluation/mlogloss.jl
@@ -1,10 +1,10 @@
 
 @doc """ multi-class logarithmic loss
 """ ->
-function mlogloss{T<:AbstractFloat}(pd::Array{T,2},gt::Array{T,2})
+function mlogloss{T<:AbstractFloat}(pd::Array{T,2}, gt::Array{T,2})
     # T can't be Real since raised an integer x to a negative power -n.
     function max_min(x::T)
-        max(min(x,1-10.0^(-15)),10^(-15))
+        max(min(x,1-10.0^(-15)),10.0^(-15))
     end
     pd = map(max_min, pd)
     log_pd = log(pd)
--- test/evaluation/mlogloss.jl
+++ test/evaluation/mlogloss.jl
@@ -1,6 +1,6 @@
 
 # test mlogloss
-let pd = [0.0 0 0 1;1 0 0 0], gt = [0.0 0 0 1;1 0 0 0]
+let pd = [0.0 0.0 0.0 1.0;1.0 0.0 0.0 0.0], gt = [0.0 0.0 0.0 1.0;1.0 0.0 0.0 0.0]
 
     @test AutoML.mlogloss(pd,gt) == 1
 
