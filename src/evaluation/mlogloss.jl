4f0bc6fd04529d6e23462df38cf1dd487b000660
guolanting <guolanting@haplox.com>
2016-01-05 03:21:56 +0000

    AbstractFloat from Real

--- src/evaluation/mlogloss.jl
+++ src/evaluation/mlogloss.jl
@@ -1,7 +1,8 @@
 
 @doc """ multi-class logarithmic loss
 """ ->
-function mlogloss{T<:Real}(pd::Array{T,2},gt::Array{T,2})
+function mlogloss{T<:AbstractFloat}(pd::Array{T,2},gt::Array{T,2})
+    # T can't be Real since raised an integer x to a negative power -n.
     function max_min(x::T)
         max(min(x,1-10.0^(-15)),10^(-15))
     end
